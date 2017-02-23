a9c8053a1cd632976e2eceb718eb2febcb4bfa10
Diego Aparicio <diego.aparicio.16@ucl.ac.uk>
2017-02-23 04:01:47 +0000

    Replace constants by configuration file. This might be the most elegant way of doing it. I'll come back to this if I have time.

--- boids.py
+++ boids.py
@@ -8,32 +8,34 @@ from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
-# A bit of tidying up
+# Replace constants by configuration file. This might be the most elegant way of doing it. I'll come back to this if I have time.
 
-boid_number = 50
+import yaml
+config=yaml.load(open("config.yaml")) 
 
-x_position_min = -450.0
-x_position_max = 50.0
-y_position_min = 300.0
-y_position_max = 600.0
+boid_number = config["boid_number"]
 
-x_velocity_min = 0
-x_velocity_max = 10.0
-y_velocity_min = -20.0
-y_velocity_max = 20.0
+x_position_min = config["x_position_min"]
+x_position_max = config["x_position_max"]
+y_position_min = config["y_position_min"]
+y_position_max = config["y_position_max"]
 
-move_to_middle_strength = 0.01
-alert_distance = 100
-formation_flying_distance = 10000
-formation_flying_strength = 0.125
+x_velocity_min = config["x_velocity_min"]
+x_velocity_max = config["x_velocity_max"]
+y_velocity_min = config["y_velocity_min"]
+y_velocity_max = config["x_velocity_max"]
 
-# variables for the plot and animation
-x_axis_min = -500
-x_axis_max = 1500
-y_axis_min = -500
-y_axis_max = 1500
-animation_frames = 50
-animation_interval = 50
+move_to_middle_strength = config["move_to_middle_strength"]
+alert_distance = config["alert_distance"]
+formation_flying_distance = config["formation_flying_distance"]
+formation_flying_strength = config["formation_flying_strength"]
+
+x_axis_min = config["x_axis_min"]
+x_axis_max = config["x_axis_max"]
+y_axis_min = config["y_axis_min"]
+y_axis_max = config["y_axis_max"]
+animation_frames = config["animation_frames"]
+animation_interval = config["animation_interval"]
 
 x_positions=[random.uniform(x_position_min,x_position_max) for boid_index in range(boid_number)]
 y_positions=[random.uniform(y_position_min,y_position_max) for boid_index in range(boid_number)]
@@ -41,29 +43,30 @@ x_velocities=[random.uniform(x_velocity_min,x_velocity_max) for boid_index in ra
 y_velocities=[random.uniform(y_velocity_min,y_velocity_max) for boid_index in range(boid_number)]
 boids=(x_positions,y_positions,x_velocities,y_velocities)
 
+
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]+=(xs[j]-xs[i])*move_to_middle_strength/len(xs)
-			yvs[i]+=(ys[j]-ys[i])*move_to_middle_strength/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < alert_distance:
-				xvs[i]+=(xs[i]-xs[j])
-				yvs[i]+=(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < formation_flying_distance:
-				xvs[i]+=(xvs[j]-xvs[i])*formation_flying_strength/len(xs)
-				yvs[i]+=(yvs[j]-yvs[i])*formation_flying_strength/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]+=xvs[i]
-		ys[i]+=yvs[i]
+    xs,ys,xvs,yvs=boids
+    # Fly towards the middle
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            xvs[i]+=(xs[j]-xs[i])*move_to_middle_strength/len(xs)
+            yvs[i]+=(ys[j]-ys[i])*move_to_middle_strength/len(xs)
+    # Fly away from nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < alert_distance:
+                xvs[i]+=(xs[i]-xs[j])
+                yvs[i]+=(ys[i]-ys[j])
+    # Try to match speed with nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < formation_flying_distance:
+                xvs[i]+=(xvs[j]-xvs[i])*formation_flying_strength/len(xs)
+                yvs[i]+=(yvs[j]-yvs[i])*formation_flying_strength/len(xs)
+    # Move according to velocities
+    for i in range(len(xs)):
+        xs[i]+=xvs[i]
+        ys[i]+=yvs[i]
 
 
 figure=plt.figure()
