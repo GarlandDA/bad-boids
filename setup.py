54f5cfb028b63d6f2587ba18f0a54c35fa00448f
Diego Aparicio <diego.aparicio.16@ucl.ac.uk>
2017-02-24 09:31:17 +0000

    pip installation, please work... part 2

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
 	scripts = ['scripts/boids'],
+	include_package_data=True,
 	package_data = {'bad_boids': ['config.yaml']},
     install_requires = ['matplotlib', 'setuptools'] # for some reason, the installer didn't like me adding 'yaml' and 'random' in this list
 )
